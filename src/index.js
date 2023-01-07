a0ecab9fd5e038efe209afba6588811551499799
elmerMg0 <mendozagutierrezelmer20@gmail.com>
2023-01-07 14:57:43 -0400

    slider dishes and menu craeted

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ window.addEventListener("scroll",()=>{
 })
 
 /* Carousel */
+const containerSlider = document.querySelector(".container-slider");
 const slider = document.querySelector(".slider");
 const sections = document.querySelectorAll(".slider__section");
 
@@ -27,23 +28,23 @@ slider.insertAdjacentElement("afterbegin",lastElement);
 const next = () => {
     let firstElement = document.querySelectorAll(".slider__section")[0];
     slider.style.marginLeft = "-200%";
-    slider.style.transition = "all 1.5s";
+    slider.style.transition = "all 1s";
     setTimeout(()=>{
         slider.style.transition = "none";
         slider.insertAdjacentElement("beforeend",firstElement);
         slider.style.marginLeft = "-100%";
-    },1500)
+    },1000)
 }
 
 const previus = () => {
     let lastElement = document.querySelectorAll(".slider__section")[sections.length-1];
     slider.style.marginLeft = "0%";
-    slider.style.transition = "all 1.5s";
+    slider.style.transition = "all 1s";
     setTimeout(()=>{
         slider.style.transition = "none";
         slider.insertAdjacentElement("afterbegin",lastElement);
         slider.style.marginLeft = "-100%";
-    },1500)
+    },1000)
 }
 
 btnRigth.addEventListener("click",()=>{
@@ -55,27 +56,97 @@ btnLeft.addEventListener("click",()=>{
 
 a = setInterval(()=>{
     next();
+},10000)
+
+containerSlider.onmouseout = function() {
+    console.log("fuera")
+    a = setInterval(()=>{
+        next();
+    },10000)
+}
+
+containerSlider.onmouseover = function(){
+    console.log("dentro")
+    clearInterval(a)
+}
+/* Carosel menu */
+const slider_menu = document.querySelector(".slider__menu");
+const container_slider_menu = document.querySelector(".container-slider-menu");
+const sectionsMenu = document.querySelectorAll(".slider__section--menu");
+
+const lastElementMenu = sectionsMenu[sections.length - 1];
+const btnRigthMenu = document.querySelector(".slider__btn-menu--rigth");
+const btnLeftMenu = document.querySelector(".slider__btn-menu--left");
+var m;
+
+slider_menu.insertAdjacentElement("afterbegin",lastElementMenu);
+
+const nextMenu = () => {
+    let firstElement = document.querySelectorAll(".slider__section--menu")[0];
+    slider_menu.style.marginLeft = "-200%";
+    slider_menu.style.transition = "all 1s";
+    setTimeout(()=>{
+        slider_menu.style.transition = "none";
+        slider_menu.insertAdjacentElement("beforeend",firstElement);
+        slider_menu.style.marginLeft = "-100%";
+    },1000)
+}
+
+const previusMenu = () => {
+    let lastElement = document.querySelectorAll(".slider__section--menu")[sections.length-1];
+    slider_menu.style.marginLeft = "0%";
+    slider_menu.style.transition = "all 1s";
+    setTimeout(()=>{
+        slider_menu.style.transition = "none";
+        slider_menu.insertAdjacentElement("afterbegin",lastElement);
+        slider_menu.style.marginLeft = "-100%";
+    },1000)
+}
+
+btnRigthMenu.addEventListener("click",()=>{
+    nextMenu();
+})
+btnLeftMenu.addEventListener("click",()=>{
+    previusMenu();
+})
+
+m = setInterval(()=>{
+    nextMenu();
 },5000)
 
-btnLeft.onmouseout = function() {
-    a = setInterval(()=>{
-        next();
-    },5000)
-}
-btnRigth.onmouseout = function() {
-    a = setInterval(()=>{
-        next();
+container_slider_menu.onmouseout = function() {
+    m = setInterval(()=>{
+        nextMenu();
     },5000)
 }
 
-
-
-btnLeft.onmouseover = function(){
-    console.log("dentro");
-    clearInterval(a)
+container_slider_menu.onmouseover = function(){
+    clearInterval(m)
 }
 
-btnRigth.onmouseover = function(){
-    console.log("dentro");
-    clearInterval(a)
-}
+/* Botones menu */
+
+const btnPlatos = document.querySelector(".menu-txt__btn-rigth");
+const btnFondos = document.querySelector(".menu-txt__btn-left");
+const menuTextPlates = document.querySelector(".menu-text-plates");
+const menuTextFondo  = document.querySelector(".menu-text-fondos");
+const dish_content  = document.querySelectorAll(".dish-content");
+
+btnPlatos.addEventListener("click",()=>{
+    const res = btnPlatos.classList.contains("btn-active")
+    if(!res){
+        btnFondos.classList.remove("btn-active");
+        btnPlatos.classList.add("btn-active");
+        menuTextPlates.classList.remove("hide-menu");
+        menuTextFondo.classList.add("hide-menu");
+    }
+})
+btnFondos.addEventListener("click",()=>{
+    const res = btnFondos.classList.contains("btn-active")
+    if(!res){
+        btnPlatos.classList.remove("btn-active");
+        btnFondos.classList.add("btn-active");
+        menuTextFondo.classList.remove("hide-menu");
+        menuTextPlates.classList.add("hide-menu");
+    }
+})
